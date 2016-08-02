dcddaa9b481229c3eb18143e3ca2c84b82f7472e
Daniel Cudney <upnvapesd@gmail.com>
2016-08-02 11:40:07 -0700

    Environment Set Up
    Environment is set up to begin editing the admin panel's user portion.

--- app/scripts/controllers/admin.usersCtrl.js
+++ app/scripts/controllers/admin.usersCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module("lionHeart")
 .controller("admin.usersCtrl", function($scope, dataService) {
-dataService.getUsers(function(response) {
-$scope.users = response.data.users;
-});
+  dataService.getUsers(function(response) {
+    $scope.users = response.data.users;
+  });
 });
--- public/scripts/lion.bundle.js
+++ public/scripts/lion.bundle.js
@@ -6413,9 +6413,9 @@ webpackJsonp([0],[
 	'use strict';
 	angular.module("lionHeart")
 	.controller("admin.usersCtrl", function($scope, dataService) {
-	dataService.getUsers(function(response) {
-	$scope.users = response.data.users;
-	});
+	  dataService.getUsers(function(response) {
+	    $scope.users = response.data.users;
+	  });
 	});
 
 
