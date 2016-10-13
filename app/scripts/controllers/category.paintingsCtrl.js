c93b9dbba0907f496c1f3abacc2692de02ad8066
Dman89 <upnvapesd@gmail.com>
2016-10-13 14:47:02 -0700

    Removed Unnecessary Files

--- app/scripts/controllers/category.paintingsCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module("lionHeart")
-.controller("category.paintingsCtrl", function($scope, dataService) {
-  dataService.getCategoryPaintings(function(response) {
-    $scope.products = response.data.products;
-  });
-});
--- app/scripts/controllers/category.upcycleCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module("lionHeart")
-.controller("category.upcycleCtrl", function($scope, dataService) {
-  dataService.getCategoryUpcycle(function(response) {
-    $scope.products = response.data.products;
-  });
-});
