48e6212f14cdb3d4c1019ef5ee5b8bd5362a2f7e
Dman89 <upnvapesd@gmail.com>
2016-10-15 13:05:54 -0700

    Admin Panel Styled (CSS)

--- public/scripts/lion.bundle.js
+++ public/scripts/lion.bundle.js
@@ -6839,8 +6839,31 @@ webpackJsonp([0],[
 
 	'use strict';
 	angular.module("lionHeart")
-	.controller("adminCtrl", function($scope, dataService) {
+	.controller("adminCtrl", function($scope, dataService, $location, $parse) {
 
+	  var searchForCurrentLocation = function(cb) {
+	    let searchThis = $location.path();
+	    let checkVariableForAdmin = searchThis.search('admin');
+	    if (checkVariableForAdmin >= 0) {
+	      let arrayOfItems = ["post", "user", "product", "port"];
+	      arrayOfItems.map(function(term) {
+	        let checkVariable = searchThis.search(term);
+	        if (checkVariable >= 0) {
+	          let scopeSaving = $parse(term+'Button');
+	          scopeSaving.assign($scope, true);
+	        }
+	        else {
+	          let scopeSavingFalse = $parse(term+'Button');
+	          scopeSavingFalse.assign($scope, false);
+	        }
+	      })
+	    }
+	    else {
+	      window.removeEventListener('hashchange', searchForCurrentLocation)
+	    }
+	  }
+	  searchForCurrentLocation();
+	  window.addEventListener('hashchange', searchForCurrentLocation)
 	});
 
 
